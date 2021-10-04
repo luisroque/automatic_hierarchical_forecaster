c04c9de4a5e670b569819a2e1aae2cc24c362218
Luis Roque <luis_roque@live.com>
2021-10-04 19:12:02 +0100

    m5 exp

---
+++ run_experiments_m5.py
@@ -0,0 +1,79 @@
+import warnings
+import pandas as pd
+import arviz as az
+
+import os, sys
+dir2 = os.path.abspath('')
+dir1 = os.path.dirname(dir2)
+if not dir1 in sys.path: sys.path.append(dir1)
+import calendar
+from libs.metrics import calculate_metrics, metrics_to_table, metrics_to_latex
+from libs.pre_processing import generate_groups_data_flat, generate_groups_data_matrix
+from libs.visual_analysis import visualize_fit, visualize_predict, traceplot, visualize_prior, model_graph, plot_elbo
+from libs.model_minibatch_series import HGPforecaster, PiecewiseLinearChangepoints
+import numpy as np
+az.style.use('arviz-darkgrid')
+warnings.filterwarnings('ignore')
+
+import theano
+theano.config.compute_test_value='raise'
+
+# Read in the data
+INPUT_DIR = '../benchmark/m5-data'
+cal = pd.read_csv(f'{INPUT_DIR}/calendar.csv')
+stv = pd.read_csv(f'{INPUT_DIR}/sales_train_validation.csv')
+ss = pd.read_csv(f'{INPUT_DIR}/sample_submission.csv')
+sellp = pd.read_csv(f'{INPUT_DIR}/sell_prices.csv')
+
+# Transform column wide days to single column 
+
+stv = stv.melt(list(stv.columns[:6]), var_name='day', value_vars=list(stv.columns[6:]), ignore_index=True)
+
+# Group by the groups to consider (remove product_id as there are 3049 unique) 
+
+stv = stv.groupby(['dept_id', 'cat_id', 'store_id', 'state_id', 'item_id', 'day']).sum('value').reset_index()
+
+days_calendar = np.concatenate((stv['day'].unique().reshape(-1,1), cal['date'][:-56].unique().reshape(-1,1)), axis=1)
+df_caldays = pd.DataFrame(days_calendar, columns = ['day','Date'])
+
+# Add calendar days
+
+stv = stv.merge(df_caldays, how='left', on='day')
+
+stv['Date'] = stv['Date'].astype('datetime64[ns]')
+stv.dtypes
+
+# stv = stv.loc[stv['dept_id']=='FOODS_1']
+
+# Transform in weekly data
+
+stv_weekly = stv.groupby(['dept_id', 'store_id', 'state_id', 'item_id']).resample('W', on='Date')['value'].sum()
+
+# Build the structure to then apply the grouping transformation
+
+stv_pivot = stv_weekly.reset_index().pivot(index='Date',columns=['dept_id', 'store_id', 'state_id', 'item_id'], values='value')
+stv_pivot = stv_pivot.fillna(0)
+
+groups_input = {
+    'Department': [0],
+    'Store': [1],
+    'State': [2],
+    'Item': [3]
+}
+
+# The dataset results from the removal of item_id (groupby by the ohter groups) and by downsampling to weekly data (the dataset was daily)
+
+groups = generate_groups_data_flat(stv_pivot, groups_input, seasonality=52, h=18)
+
+# Instantiate the model class
+m = HGPforecaster(groups_data=groups,
+                  n_iterations=100000,
+                  changepoints = 4,
+                  piecewise_out=True)
+
+# Fit and predict
+m.fit_vi()
+
+m.predict()
+
+results = calculate_metrics(m.pred_samples_predict, groups)
